a6196c2832806c53301d159621226b4b9305705a
jagadeeshan <jagadeeshanmssj@gmail.com>
2022-06-08 23:06:28 +0530

    ros noetic compatability

--- src/rqt_launchtree/launchtree_plugin.py
+++ src/rqt_launchtree/launchtree_plugin.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 from rqt_gui_py.plugin import Plugin
 
@@ -31,20 +31,25 @@ class LaunchtreePlugin(Plugin):
         _curr_pkg = self._widget.package_select.currentText()
         rospy.logdebug('save_settings) currentIndex={}'.format(_curr_pkg))
         instance_settings.set_value(self._SETTING_LASTPKG, _curr_pkg)
-        instance_settings.set_value(self._SETTING_LASTLAUNCHFILE, self._widget.launchfile_select.currentText())
-        instance_settings.set_value(self._SETTING_LASTLAUNCHARGS, self._widget.args_input.text())
+        instance_settings.set_value(
+            self._SETTING_LASTLAUNCHFILE, self._widget.launchfile_select.currentText())
+        instance_settings.set_value(
+            self._SETTING_LASTLAUNCHARGS, self._widget.args_input.text())
 
     def restore_settings(self, plugin_settings, instance_settings):
         self._widget.editor = instance_settings.value('editor', 'gedit')
-        self._widget.args_input.setText(instance_settings.value(self._SETTING_LASTLAUNCHARGS, ''))
-        pkg_idx = self._widget.package_select.findText(instance_settings.value(self._SETTING_LASTPKG))
+        self._widget.args_input.setText(
+            instance_settings.value(self._SETTING_LASTLAUNCHARGS, ''))
+        pkg_idx = self._widget.package_select.findText(
+            instance_settings.value(self._SETTING_LASTPKG))
         if pkg_idx >= 0:
             self._widget.package_select.blockSignals(True)
             self._widget.package_select.setCurrentIndex(pkg_idx)
             self._widget.package_select.blockSignals(False)
             self._widget.update_launchfiles(pkg_idx)
             # only set launch file if pkg was restored
-            launch_idx = self._widget.launchfile_select.findText(instance_settings.value(self._SETTING_LASTLAUNCHFILE))
+            launch_idx = self._widget.launchfile_select.findText(
+                instance_settings.value(self._SETTING_LASTLAUNCHFILE))
             if launch_idx >= 0:
                 self._widget.launchfile_select.blockSignals(True)
                 self._widget.launchfile_select.setCurrentIndex(launch_idx)
@@ -54,9 +59,9 @@ class LaunchtreePlugin(Plugin):
 
     def trigger_configuration(self):
         (text, ok) = QInputDialog.getText(self._widget,
-            'Settings for %s' % self._widget.windowTitle(),
-            'Command to edit launch files (vim, gedit, ...), can accept args:',
-            text = self._widget.editor
-        )
+                                          'Settings for %s' % self._widget.windowTitle(),
+                                          'Command to edit launch files (vim, gedit, ...), can accept args:',
+                                          text=self._widget.editor
+                                          )
         if ok:
             self._widget.editor = text
